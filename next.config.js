ec742b15dbdbf42a8a84dd3636b6c389603b40ee
Ludoviko <me@ludoviko.xyz>
2021-07-23 17:28:40 +0100

    Added redirect and blank index

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/ugxv4ZrVjC',
+        permanent: true
+      }
+    ]
+  }
 }
