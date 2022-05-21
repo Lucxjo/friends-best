ca7034738b6da70dc7dd92b08b5557f8001906d9
Ludoviko <me@ludoviko.ch>
2022-05-21 21:28:41 +0100

    Started the styling
    Need to look into how to properly deal with styling in nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	css: [
-		'@/styles/globals.css'
+		'@/assets/styles/globals.scss',
+		'@/assets/styles/index.scss',
 	]
 });
