0010bc5d428f5d56d71bd93aaa41b6ff6e9077a4
Ludoviko <me@ludoviko.ch>
2022-05-22 17:51:25 +0100

    Files changed for docker cleanup
    Need to optimise docker builds but that can wait

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,5 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	css: ['@/assets/styles/globals.scss'],
-	buildModules: ['@intlify/nuxt3'],
-	intlify: {
-		vueI18n: {
-			availableLocales: ['en', 'eo'],
-		}
-	}
+	css: ['@/assets/styles/globals.scss']
 });
