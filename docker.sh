9241dc0783ff8ba1d6dc1195f6129339e41cf5fc
Louis Hollingworth <louis@hollingworth.ch>
2022-10-01 19:35:07 +0100

    Switch to Astro
    Signed-off-by: Louis Hollingworth <louis@hollingworth.ch>

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-m=$(cat ./build_number)
-echo $m
-wait 10
-docker build -t ghcr.io/lucxjo/friends-best:build-$m -t ghcr.io/lucxjo/friends-best:latest .
-docker push ghcr.io/lucxjo/friends-best:build-$m
-docker push ghcr.io/lucxjo/friends-best:latest
-
-echo $((m+1)) > ./build_number
